00bb7d31104fca0421af94cb50027ae2478d3793
AarshShah9 <aarshshah99@gmail.com>
2023-04-18 14:40:51 -0600

    adding to the video sender script

--- backend/main.py
+++ backend/main.py
@@ -1,33 +1,46 @@
 import ffmpeg
+import cv2
 
 
 def main():
-    width = 640
-    height = 480
-    framerate = 30
-    bitrate = '1M'
 
-    stream_url = 'rtmp://<server-address>/<stream-name>'
+    # width = 640
+    # height = 480
+    # framerate = 30
 
+    # Set up video capture
+    cap = cv2.VideoCapture(0, cv2.CAP_DSHOW)
+    width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+    height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    framerate = int(cap.get(cv2.CAP_PROP_FPS))
+
+    # Set up ffmpeg input and output parameters
     input_params = {
         'video_size': f'{width}x{height}',
         'framerate': str(framerate),
-        'pixel_format': 'yuyv422',
+        'pixel_format': 'bgr24',
         'input_format': 'rawvideo'
     }
+
+    stream_url = 'rtmp://<server-address>/<stream-name>'
 
     output_params = {
         'pix_fmt': 'yuv420p',
         'preset': 'veryfast',
-        'b:v': bitrate,
+        'b:v': '1M',
         'f': 'flv'
     }
 
-    stream = ffmpeg.input('video0', format='v4l2', **input_params)
+    # Set up ffmpeg input and output streams
+    # stream = ffmpeg.input('video0', format='v4l2', **input_params) # raspberry pi
+
+    stream = ffmpeg.input('video=Camera 0', format='v4l2',
+                          **input_params)  # windows
     stream = ffmpeg.output(stream, stream_url, **output_params)
 
+    # Start streaming video frames to AWS server
     ffmpeg.run(stream)
 
 
-if "__main__" == __name__:
+if __name__ == "__main__":
     main()
